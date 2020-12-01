fcce733ed5c7937da0160dda173d4f8e30fb9115
Abdelqader Okasha <abd.okasha@gmail.com>
2020-12-01 23:49:36 +0200

    Added more cases to ALUTest

--- testbenches/ALUTest.v
+++ testbenches/ALUTest.v
@@ -12,44 +12,64 @@ module ALUTest;
     reg [31:0] cycle;
     
     initial begin
-		PC_VALUE_ <= 0;	  
-		cycle <= 1;
+		//PC_VALUE_ <= 0;	  
+		cycle <= 0;
+
+		
 	end	
 
-	always @(posedge top.clk) begin	
-/* Control Signals
-0000 AND
-0001 OR
-0010 Unsigned Addition
-0011 Unsigned Subtraction
-0100 Set Less Than
-0101 Set Less Than Unsigned
-0111 NOR
-1000
-1001
-1010 Signed Addition
-1011 Signed Subtraction
-1100 Unsigned Multiply 
-1101 Unsigned Divide 
-1110 Signed Multiply 
-1111 Signed Divide 
-*/
-        case(cycle)
-        1: begin //0000 AND
-           control = 4'b0000;
-           in1 = 32'h55555555; // (0101 0101 0101 0101 0101 0101 0101 0101)
-           in2 = 32'h000000F0; // (0000 0000 0000 0000 0000 0000 1111 0000)
-
-           $display("result = : %h" , out, " The correct value is 50"); 
+	always @(posedge clk) begin	
+	/* Testing for all Control Signals
+	0000 AND
+	0001 OR
+	0010 Unsigned Addition
+	0011 Unsigned Subtraction
+	0100 Set Less Than
+	0101 Set Less Than Unsigned
+	0111 NOR
+	1000
+	1001
+	1010 Signed Addition
+	1011 Signed Subtraction
+	1100 Unsigned Multiply 
+	1101 Unsigned Divide 
+	1110 Signed Multiply 
+	1111 Signed Divide 
+	*/
+	  case(cycle)
+        
+		  0: begin //0000 AND
+				control = 4'b0000;
+				in1 = 32'h55555555; // (0101 0101 0101 0101 0101 0101 0101 0101)
+				in2 = 32'h000000F0; // (0000 0000 0000 0000 0000 0000 1111 0000)
+				$strobe("result = : %h" , out, " The correct value is 00000050 | z = %b and o = %b", z,o); 
+		  end
+		  
+		  1: begin //0001 OR
+				control = 4'b0001;
+				in1 = 32'h00000055; // (0000 0000 0000 0000 0000 0000 0101 0101)
+				in2 = 32'h000000A0; // (0000 0000 0000 0000 0000 0000 1010 0000)
+				$strobe("result = : %h" , out, " The correct value is 000000F5 | z = %b and o = %b", z,o); 
+		
         end
-        2: begin
-            
+        2: begin //0010 Unsigned Addition with overflow
+            control = 4'b0010;
+				in1 = 32'h80000055; // (1000 0000 0000 0000 0000 0000 0101 0101)
+				in2 = 32'h80000001; // (1000 0000 0000 0000 0000 0000 0000 0001)
+				$strobe("result = : %h" , out, " The correct value is 00000056 | overflow should occur! z = %b and o = %b", z,o); 
+				
         end
-        3: begin
-            
+        3: begin //0011 Unsigned Subtraction with overflow
+            control = 4'b0011;
+				in1 = 32'h00000055; // (0000 0000 0000 0000 0000 0000 0101 0101)
+				in2 = 32'h00000001; // (0000 0000 0000 0000 0000 0000 0000 0001)
+				$strobe("result = : %h" , out, " The correct value is FFFFFFAC | overflow should occur! z = %b and o = %b", z,o); 
         end
-        4: begin
-            
+        4: begin //0011 Unsigned Subtraction with zero
+            control = 4'b0011;
+				in1 = 32'hF0FF0055; // (1111 0000 1111 1111 0000 0000 0101 0101)
+				in2 = 32'hF0FF0055; // (1111 0000 1111 1111 0000 0000 0101 0101)
+				$strobe("result = : %h" , out, " The correct value is 00000000 | zero should occur! z = %b and o = %b", z,o); 
         end
         5: begin
             
@@ -75,10 +95,10 @@ module ALUTest;
         12: begin
             
         end
-        default:   $finish; ;
+        default:   $stop; 
 
 
-        endcase
+	  endcase
         
       
         
